13d3cae398be130a6ce87ac4dbf139219d746f44
robertabella <robertabella@192.168.0.20>
2020-03-24 11:46:06 +0800

    update regression test suite name

---
+++ Test Suites/Regression Test Suite.ts
@@ -0,0 +1,217 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>50c0fa2d-6c49-4e45-bd55-f3d02ad2df9c</testSuiteGuid>
+   <testCaseLink>
+      <guid>215dae71-8480-4015-8a1c-f813340b2756</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001_Invalid Email Address</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e90ec55f-3248-4da9-9dd5-958fe8f8ca20</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_002_Incorrect Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>58acfc6f-e1a2-4059-b56f-e4bdae47bb23</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_003_Verify Forgot Password (Valid Email)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6fe8de3-e645-4092-9162-283c0599038c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_009_Verify Sort By Z-A Oldest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e191af59-e2aa-44de-8f18-186ed28c90da</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_011_Verify Email CSV Report</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>78047a4b-88a3-45e2-8add-aca9b2981753</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_034_Verify Training and Support</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f102ccfb-e662-4ae8-83ae-f3fce43ba053</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_029_ Verify Deduct Lunch Time</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b5e242cd-2b65-4ebb-8519-3f45c00afceb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_019_Display Non Working Days (ON)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>be928ba4-ff16-4a34-8516-f6fd7de74521</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_016_Track Leave Early (OFF)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dee9a5de-383b-4deb-ac13-b8e377f3fb98</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_032_Verify Account Administrator Email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a532b148-b7f7-43d1-83a8-a6b8e77b1c45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_013_Track Late Arrival (ON)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ece562da-94c9-44a4-be20-746cd7ce7bf5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_021_Display Tag for - Sick, Vacation, Personal (ON)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0fb6f95f-0c8b-491b-ab00-2ac8a761b3b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_025_Verify Employee Grace Period (Late Arrival)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>780d3137-287d-4e50-a91c-a5751a41d35f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_006_Verify_Apply Date Range</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>88884c7b-91df-43c8-9879-1f602458cb01</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_030_Verify Deduct Break Time</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1ac487db-be7d-4068-8d4a-de3b2676b7bc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_028_Verify Pay Period</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d9362da8-198d-4c9d-ba62-f5174c1db727</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_023_Allow Individualized Work Schedules (ON)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2cdd82c9-3592-4191-bbd4-2cbbfb3fc8d9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_026_Verify Company Work Days</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7f3324e2-6028-444c-ac1b-2d3615a160eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_014_Track Late Arrival (OFF)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7a67d583-2f90-4758-8898-fb3003cfa23c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_027_Verify Company Work Time</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d5cc0644-b585-4918-bbd6-bcd7db5fa110</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_007_Enable Timesheet changes (ON)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f6f48673-cc97-4dfa-a6ce-a38ccbe3bddb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_005_Successful Admin Login account</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b1518eb4-a94b-44a8-8f58-f54a8bb580c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_031_Verify Auto-Send Reports To</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>47740b34-7a60-429b-97bd-5bfd274591dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_010_Verify Sort By A - Z Newest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>14e1b323-630f-472b-b934-28c2d30e2f00</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_022_Display Tag for - Sick, Vacation, Personal (OFF)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a1185fc0-d4a4-4b02-b29a-ca07d72b81dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_033_Verify Billing and Payments</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b4855b71-52d7-4a63-a0a8-03bc87bc1ae1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_015_Track Leave Early (ON)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>76790d49-af10-4957-918b-2a6a2e4e36fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_008_Enable Timesheet changes (OFF)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>155ffb23-363c-4b83-a1b9-b556da511694</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_018_Display Return of Time Deducted (OFF)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>de2a2abb-ce6a-4534-803b-a19d98a0973b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_017_Display Return of Time Deducted (ON)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2d5a6d5f-f958-419f-a7d1-fdf379c7e873</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_020_Display Non Working Days (OFF)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b2b68c37-0e1c-471a-9951-09b4825d41d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_012_Add New Employee</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3b243d44-eb4e-4f8a-846a-b811823443e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_024_Allow Individualized Work Schedules (OFF)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a2e637e7-8de6-4a2d-ba79-a59bb3e82bed</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_004_Verify Forgot Password (Invalid Email)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
